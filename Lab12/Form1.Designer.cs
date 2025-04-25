a8716eb6ee96f635b45b56d4dc7fbbc7f383f58a
Your Name <youremail@yourdomain.com>
2025-04-25 21:11:20 +0530

    Lab12

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace AlarmClockApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtTimeInput;
+        private System.Windows.Forms.Button btnStart;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtTimeInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTimeInput
+            // 
+            this.txtTimeInput.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtTimeInput.Location = new System.Drawing.Point(30, 30);
+            this.txtTimeInput.Name = "txtTimeInput";
+            this.txtTimeInput.Text = "HH:MM:SS";
+            this.txtTimeInput.ForeColor = System.Drawing.Color.Gray;
+            this.txtTimeInput.GotFocus += (s, e) => {
+                if (txtTimeInput.Text == "HH:MM:SS")
+                {
+                    txtTimeInput.Text = "";
+                    txtTimeInput.ForeColor = System.Drawing.Color.Black;
+                }
+            };
+            this.txtTimeInput.LostFocus += (s, e) => {
+                if (string.IsNullOrWhiteSpace(txtTimeInput.Text))
+                {
+                    txtTimeInput.Text = "HH:MM:SS";
+                    txtTimeInput.ForeColor = System.Drawing.Color.Gray;
+                }
+            };
+            this.txtTimeInput.Size = new System.Drawing.Size(200, 29);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnStart.Location = new System.Drawing.Point(250, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 30);
+            this.btnStart.Text = "Start Alarm";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 100);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTimeInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Alarm Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Lab12/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AlarmClockApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private DateTime targetTime;
+        private bool isAlarmSet = false;
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+
+            btnStart.Click += BtnStart_Click;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (DateTime.TryParse(txtTimeInput.Text, out targetTime))
+            {
+                isAlarmSet = true;
+                MessageBox.Show($"Alarm set for {targetTime.ToLongTimeString()}");
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Invalid time format. Please enter time in HH:MM:SS.");
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!isAlarmSet)
+                return;
+
+            this.BackColor = GetRandomColor();
+
+            DateTime now = DateTime.Now;
+            if (now.Hour == targetTime.Hour &&
+                now.Minute == targetTime.Minute &&
+                now.Second == targetTime.Second)
+            {
+                timer.Stop();
+                isAlarmSet = false;
+                MessageBox.Show("⏰ Alarm! Time's up!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private Color GetRandomColor()
+        {
+            return Color.FromArgb(255,
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+        }
+    }
+}
